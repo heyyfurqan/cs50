c3f2963aef24fff6dc3cbe852e0fb9d7f4e9729c
Furqan Abid <thisismefurqan@gmail.com>
2022-04-21 22:04:31 -0700

    Add variable z

--- Week_1/Lecture/calculator.c
+++ Week_1/Lecture/calculator.c
@@ -5,5 +5,6 @@ int main(void)
 {
     int x = get_int("Enter value of x: \n");
     int y = get_int("Enter value of y: \n");
-    printf("The sum of x and y is : %i \n", x + y ); // %i is the placeholder for a integer and x+y defines it
+    int z = x + y;
+    printf("The sum of x and y is : %i \n", z ); // %i is the placeholder for a integer and x+y defines it
 }
